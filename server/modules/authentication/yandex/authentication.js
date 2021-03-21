5433a8814047bf0190c4056782607653f54a8c19
Andrey Sinitsyn <andrey.sin98@gmail.com>
2021-03-21 20:01:11 +0300

    feat: add Yandex authentication

---
+++ server/modules/authentication/yandex/authentication.js
@@ -0,0 +1,41 @@
+/* global WIKI */
+
+// ------------------------------------
+// Yandex Account
+// ------------------------------------
+
+const YandexStrategy = require('./strategy')
+const _ = require('lodash')
+
+module.exports = {
+  init (passport, conf) {
+    passport.use('yandex',
+      new YandexStrategy({
+        clientID: conf.clientId,
+        clientSecret: conf.clientSecret,
+        callbackURL: conf.callbackURL,
+        passReqToCallback: true
+      }, async (req, accessToken, refreshToken, profile, cb) => {
+        if (profile._json.default_email.split('@')[1] !== conf.authenticationDomain)
+          return cb(new Error('Authentication domain does not match!'), null)
+        try {
+          const user = await WIKI.models.users.processProfile({
+            providerKey: req.params.strategy,
+            profile: {
+              ...profile,
+              diplayName: profile._json.real_name,
+              email: profile._json.default_email,
+              picture: profile._json.is_avatar_empty
+              ? undefined
+              : profile.photos.find((item) => item.type === "thumbnail")
+                  .value,
+            }
+          })
+          cb(null, user)
+        } catch (err) {
+          cb(err, null)
+        }
+      }
+      ))
+  }
+}
